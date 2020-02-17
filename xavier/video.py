753d875c4c6f8e7eef3a5dec2d509fe74654ad20
Jerry Yu <jerryyu8@gmail.com>
2020-02-17 01:07:12 -0500

    Add timing code and move process to fn

--- xavier/video.py
+++ xavier/video.py
@@ -2,6 +2,7 @@ import sys
 import cv2
 import os
 import argparse
+import time
 
 # Location of OpenPose python binaries
 openpose_path = '../../openpose'
@@ -10,6 +11,8 @@ sys.path.append(openpose_python_path)
 
 from openpose import pyopenpose as op
 
+# Initialize OpenPose
+# TODO: tune flags to optimize speed of openpose
 params = dict()
 params["model_folder"] = openpose_path + "/models/"
 
@@ -19,14 +22,18 @@ opWrapper.start()
 
 img_path = openpose_path + "/examples/media/COCO_val2014_000000000192.jpg"
 
-# Process Image
-datum = op.Datum()
+# Process Image, expects cv2 image
+def processImage(img):
+    datum = op.Datum()
+    datum.cvInputData = img
+    opWrapper.emplaceAndPop([datum])
+    cv2.imwrite("out.jpg", datum.cvOutputData)
+    return datum.poseKeypoints
+
 imageToProcess = cv2.imread(img_path)
-print(imageToProcess)
-print(imageToProcess.shape)
-
-
-datum.cvInputData = imageToProcess
-opWrapper.emplaceAndPop([datum])
-
-print("Body keypoints: \n" + str(datum.poseKeypoints))
+print("Image shape", imageToProcess.shape)
+start = time.time()
+keypoints = processImage(imageToProcess)
+end = time.time()
+#print("Body keypoints: \n" + str(keypoints))
+print("Total time: " + str(end - start))
