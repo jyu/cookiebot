963f404a52ab295f74dd510ebba83db7ae3e2f0a
Jerry Yu <jerryyu8@gmail.com>
2020-03-05 12:48:51 -0500

    Improve logging on server

--- server/app.js
+++ server/app.js
@@ -1,29 +1,36 @@
 const express = require("express");
 const app = express();
-const expressWs = require('express-ws')(app);
+const expressWs = require("express-ws")(app);
 const port = 5000;
 
-app.get('/', (req, res) => res.send("Hi Jerry\n"));
+app.get("/", (req, res) => res.send("CookieServer Dashboard Under Construction\n"));
 
 let robot_ws = null;
 
-app.ws('/robot', function(ws, req) {
-    ws.on('open', function() {
-        console.log("Websocket Connection Opened with robot");
-    });
+app.ws("/robot", function(ws, req) {
+    console.log("Connection established with Robot");
     robot_ws = ws;
+    ws.on("message", function(msg) {
+        console.log("CookieServer <- Robot: " + msg);
+    });
+    ws.on("close", function() {
+        console.log("Connection with Robot closed");
+        robot_ws = null;
+    });
 });
 
-app.ws('/gestures', function(ws, req) {
-    ws.on('open', function() {
-        console.log("Websocket Connection Opened with cookie");
-    });
-    ws.on('message', function(msg) {
-        console.log("Got new command <- ", msg)
+app.ws("/gestures", function(ws, req) {
+    console.log("Connection established with Gestures")
+    ws.on("message", function(msg) {
+        console.log("CookieServer <- Gestures: " + msg)
         command = msg;
         if (robot_ws) {
+            console.log("CookieServer -> Robot: " + msg)
             robot_ws.send(command);
         }  
+    });
+    ws.on("close", function() {
+        console.log("Connection with Gestures closed");
     });
 });
 
