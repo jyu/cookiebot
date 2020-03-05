5a4ce15535b500e3d543b9711b82da72fe260752
Jerry Yu <jerryyu8@gmail.com>
2020-03-05 12:46:57 -0500

    Make train svm general

--- xavier/train_svm.py
+++ xavier/train_svm.py
@@ -4,16 +4,27 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import pickle
 import argparse
+import yaml
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-m') # Model name
+parser.add_argument("-c", help="config path for svm") # Config name
 args = parser.parse_args()
-model_name = args.m
+config_path = args.c
+
+# Load parameters from config
+config = yaml.load(open(config_path), Loader=yaml.Loader)
+classes = config.get('classes')
+model_path = config.get('model_path')
+data_dir = config.get('data_dir')
+# SVM params
+C = config.get('C')
+kernel = config.get('kernel')
+test_size = config.get('test_size')
+print("C:", C, "kernel:", kernel, "test_size:", test_size)
 
 # Returns data as a python list of np arr features
 def getDataFromFile(f_name):
-    data_dir = "teleop_data"
     f = open(data_dir + "/" + f_name)
     lines = f.readlines()
     data = []
@@ -27,30 +38,24 @@ def getDataFromFile(f_name):
 # Get data
 X = []
 Y = []
-left = getDataFromFile("left")
-for l in left:
-    X.append(l)
-    Y.append(0)
-straight = getDataFromFile("straight")
-for s in straight:
-    X.append(s)
-    Y.append(1)
-right = getDataFromFile("right")
-for r in right:
-    X.append(r)
-    Y.append(2)
+
+for i in range(len(classes)):
+    c = classes[i]
+    data = getDataFromFile(c)
+    for feat in data:
+        X.append(feat)
+        Y.append(i)
 
 X = np.array(X)
 Y = np.array(Y)
 print("All data shape X:", X.shape, "Y:", Y.shape)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size)
 
 print("All train data shape X_train:", X_train.shape, "Y_train:", Y_train.shape)
 print("All test data shape X_test:", X_test.shape, "Y_test:", Y_test.shape)
 
-model = SVC(probability=True, C=10, kernel="rbf")
+model = SVC(probability=True, C=C, kernel=kernel)
 model.fit(X_train, Y_train)
 
 score = model.score(X_train, Y_train)
@@ -58,5 +63,5 @@ print("Training accuracy:", score)
 score = model.score(X_test, Y_test)
 print("Test accuracy:", score)
 
-out_f = open(model_name, 'wb')
+out_f = open(model_path, 'wb')
 pickle.dump(model, out_f)
