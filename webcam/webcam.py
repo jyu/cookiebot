ff6d8d6d388c895a0110a5267157fe1f46d748cb
Jerry Yu <jerryyu8@gmail.com>
2020-03-26 11:41:57 -0700

    Add video recording

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -4,32 +4,60 @@ import asyncio
 import websockets
 import numpy as np
 import time
+import argparse
+import sys
 
 # Local webcam
 #cap = cv2.VideoCapture(0)
-# USB webcam
+
+# Probably windows USB webcam
+#cap = cv2.VideoCapture(1) 
+
+# Ubuntu USB webcam
 cap = cv2.VideoCapture("/dev/video2", cv2.CAP_V4L)
 
-async def hello():
-  uri = "ws://72.74.137.57:5000"
-  count = 0
-  start = time.time()
-  async with websockets.connect(uri) as ws:
-    while True:
-      success, frame = cap.read()
-      _, buf = cv2.imencode('.jpg', frame)
-      jpg_as_text = base64.b64encode(buf)
-      await ws.send(jpg_as_text)
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-name', default="video.mp4", help="Name of video") # Video name
+parser.add_argument('-wait', default=0, help="Seconds to wait before recording") # Wait time
+args = parser.parse_args()
 
-      jpg_as_text = await ws.recv()
-      buf = base64.b64decode(jpg_as_text)
-      img = cv2.imdecode(np.fromstring(buf, dtype=np.int8), -1)
-      cv2.imshow('frame', img)
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Out file, defaults to video.mp4
+name = args.name
+wait_time = int(args.wait)
 
-      count += 1
-      end = time.time()
-      print("FPS", count / (end - start), end="\r")
-    
-asyncio.get_event_loop().run_until_complete(hello())
+width = int(cap.get(3))
+height = int(cap.get(4))
+
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(name, fourcc, 30.0, (width, height))
+
+start = time.time()
+
+waiting = False
+if wait_time > 0:
+  waiting = True
+
+while True:
+  success, frame = cap.read()
+  
+  if waiting:
+    end = time.time()
+    elapsed = round(end - start, 2)
+    black = (0,0,0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    left = wait_time - elapsed
+    cv2.putText(frame, str(left) + " time left", (20, 20), font, .5, black, 1, cv2.LINE_AA)
+    if left <= 0:
+      waiting = False
+  else:
+    out.write(frame)
+  
+  cv2.imshow('frame', frame)
+  # Press q to exit
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+cap.release()
+out.release()
