7d684a6683453710f1cb0cb1fc60c82dd52565bc
Jerry Yu <jerryyu8@gmail.com>
2020-04-24 00:05:49 -0400

    add point tracking and map template

--- server/python/find_user_robot.py
+++ server/python/find_user_robot.py
@@ -8,6 +8,7 @@ import pickle
 import math
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import json
 
 # find openpose library
 openpose_path = "../../../openpose"
@@ -40,7 +41,7 @@ def find_robot(frame):
         y = int(y)
         cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
         output = "Robot in frame at (" + str(int(x)) + ", " + str(int(y)) + ")"
-    return x, y, output, frame
+    return [x, y], output, frame
 
 
 # coords accepts a tuple and takes precedence over x and y
@@ -56,18 +57,25 @@ def check_coords(x=None, y=None, coords=None):
     return None, None
 
 
-# find the center of the user from the given openpose keypoints
-def find_user_center(keypoints):
-    # right heel, left heel, left toes, right toes
-    relevant_keypoints = [11, 14, 19, 22]
-    x = 0
-    y = 0
-    for keypoint in relevant_keypoints:
-        x += keypoints[0][keypoint][0]
-        y += keypoints[0][keypoint][1]
-    x /= len(relevant_keypoints)
-    y /= len(relevant_keypoints)
-    return int(x), int(y)
+# find the back and front centers of the user from the given openpose keypoints
+def find_user_centers(keypoints):
+    # right heel, left heel
+    back_keypoints = [11, 14]
+    # left toes, right toes
+    front_keypoints = [19, 22]
+    back_coords = [0, 0]
+    for keypoint in back_keypoints:
+        back_coords[0] += keypoints[0][keypoint][0]
+        back_coords[1] += keypoints[0][keypoint][1]
+    front_coords = [0, 0]
+    for keypoint in front_keypoints:
+        front_coords[0] += keypoints[0][keypoint][0]
+        front_coords[1] += keypoints[0][keypoint][1]
+    # find center of back and front
+    for i in range(len(back_coords)):
+        back_coords[i] /= len(back_keypoints)
+        front_coords[i] /= len(front_keypoints)
+    return list(map(int, back_coords)), list(map(int, front_coords))
 
 
 # find the position of the user in the given frame
@@ -82,12 +90,15 @@ def find_user(opWrapper, frame, keypoints):
         keypoints, _ = get_keypoints(opWrapper, frame)
     # check if the user was found
     if np.size(keypoints) != 1:
-        center = find_user_center(keypoints)
-        x, y = check_coords(coords=center)
-        if x and y:
+        back, front = find_user_centers(keypoints)
+        back_x, back_y = check_coords(coords=back)
+        front_x, front_y = check_coords(coords=front)
+        if back_x and back_y:
             output = "User in frame at (" + str(y) + ", " + str(x) + ")"
-            cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
-    return x, y, output, frame
+            cv2.circle(frame, (back_x, back_y), 5, (0, 0, 255), -1)
+        if front_x and front_y:
+            cv2.circle(frame, (front_x, front_y), 5, (0, 0, 0), -1)
+    return [back_x, back_y], [front_x, front_y], output, frame
 
 
 # use openpose to get keypoint positions, also returning the modified frame
@@ -101,22 +112,54 @@ def get_keypoints(opWrapper, frame):
 # calculates map coordinates from mapping
 # returns position coordinates as a tuple and formatted string
 # if x or y is none, returns none for coords and an empty string
-def calc_coords(mapping, x, y):
+def calc_coords(mapping, coords):
+    x = coords[0]
+    y = coords[1]
     if x is None or y is None:
         return None, ""
     frame_coord = np.float32([[[x, y]]])
     map_coord = cv2.perspectiveTransform(frame_coord, mapping).ravel()
     output = "On map at (" + str(int(map_coord[0])) + ", " + str(int(map_coord[1])) + ")"
-    return (map_coord[0], map_coord[1]), output
+    return (map_coord[0].item(), map_coord[1].item()), output
+
+
+# converts a vector to cartesian coordinates given its slope and quadrant
+def convert_vector_coord(slope, quadrant, distance):
+    hypotenuse = np.sqrt(1 + slope ** 2)
+    triangle_ratio = distance / hypotenuse
+    x = quadrant * triangle_ratio
+    y = quadrant * triangle_ratio * slope
+    return [x, y]
+
+
+# calculates point coords relative to user map coords
+def calc_point_coords(back_coords, front_coords, grid_size, point_x, point_y):
+    # meters to millimeters
+    grid_size = 1000 * grid_size
+    x_dist = point_x * grid_size
+    y_dist = point_y * grid_size
+    direction = [front_coords[i] - back_coords[i] for i in range(2)]
+    # division by zero with floats will basically never happen
+    if (direction[0] == 0 or direction[1] == 0):
+        return None
+    slope = direction[1] / direction[0]
+    perpendicular_slope = -direction[0] / direction[1]
+    y_delta = convert_vector_coord(slope, np.sign(direction[0]), y_dist)
+    x_delta = convert_vector_coord(perpendicular_slope, np.sign(direction[1]), x_dist)
+    point_coords = [sum(coord) for coord in zip(x_delta, y_delta, back_coords)]
+    output = "Pointing at (" + str(point_x) + ", " + str(point_y) + ")"
+    return point_coords, output
+
 
 
 # writes user and robot coords onto the given frame
-def write_coords(frame, user_frame_string, user_map_string, robot_frame_string, robot_map_string):
+def write_coords(frame, user_frame_string, user_map_string, robot_frame_string, robot_map_string, point_string):
     color = (0, 0, 0)
     cv2.putText(frame, user_frame_string, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
     cv2.putText(frame, user_map_string, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
-    cv2.putText(frame, robot_frame_string, (10, 80), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
-    cv2.putText(frame, robot_map_string, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
+    cv2.putText(frame, point_string, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
+    cv2.putText(frame, robot_frame_string, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
+    cv2.putText(frame, robot_map_string, (10, 120), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 1, cv2.LINE_AA)
     return frame
 
 
@@ -151,39 +194,63 @@ def draw_axes(frame, frame_axes):
     return frame
 
 
-# draws the map with axes and user and robot positions
-def draw_map(frame, axes, user_map_coords, robot_map_coords):
+# initializes map with axes
+def init_map(map_axes):
     fig, ax = plt.subplots()
     ax.axis("equal")
     fig.suptitle("Map")
     # plot axis lines
-    x_axis = list(zip(axes[0][0], axes[0][1]))
-    y_axis = list(zip(axes[1][0], axes[1][1]))
+    x_axis = list(zip(map_axes[0][0], map_axes[0][1]))
+    y_axis = list(zip(map_axes[1][0], map_axes[1][1]))
     plt.plot(x_axis[0], x_axis[1], color="#00ff00")
     plt.plot(y_axis[0], y_axis[1], color="#0000ff")
+
+
+# draws the user and robot positions on the map and appends to frame
+# removes the position circles from the map after append
+def draw_map(frame, map_template, user_map_back_coords, user_map_front_coords, robot_map_coords, point_coords):
+    fig = plt.gcf()
+    ax = plt.gca()
     # plot user and robot positions if given
-    if user_map_coords:
-        user = plt.Circle(user_map_coords, 50, color="#0000ff")
-        ax.add_patch(user)
+    if user_map_back_coords:
+        user_back = plt.Circle(user_map_back_coords, 50, color="#0000ff")
+        ax.add_patch(user_back)
+    if user_map_front_coords:
+        user_front = plt.Circle(user_map_front_coords, 50, color="#000000")
+        ax.add_patch(user_front)
     if robot_map_coords:
         robot = plt.Circle(robot_map_coords, 50, color="#00ff00")
         ax.add_patch(robot)
+    if point_coords:
+        point = plt.Circle(point_coords, 50, color="#ff0000")
+        ax.add_patch(point)
     fig.canvas.draw()
     # write plot to image and add to frame
     map_image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     map_image = map_image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    if map_template is not None:
+        map_image = cv2.addWeighted(map_image, 0.7, map_template, 0.3, 0)
     frame = np.concatenate((frame, map_image), axis=1)
-    plt.close(fig)
+    # restore map to original
+    if user_map_back_coords:
+        user_back.remove()
+    if user_map_front_coords:
+        user_front.remove()
+    if robot_map_coords:
+        robot.remove()
+    if point_coords:
+        point.remove()
     return frame
 
 
 # scan video for user and robot and output pixel location per frame
-def scan_video(video, opWrapper, mapping, sample_rate, video_output, output, display, fast):
+def scan_video(video, opWrapper, mapping, map_file, sample_rate, points_file, video_output, display, fast):
     print("Scanning Video")
-    outfile = None
-    if output:
-        outfile = open(output, "w")
-
+    positions = []
+    points = None
+    if points_file:
+        with open(points_file, "r") as f:
+            points = json.load(f)
     capture = cv2.VideoCapture(video)
     fps = int(capture.get(cv2.CAP_PROP_FPS))
     num_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -193,8 +260,14 @@ def scan_video(video, opWrapper, mapping, sample_rate, video_output, output, dis
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         writer = cv2.VideoWriter(video_output, fourcc, fps / sample_rate, (2 * int(capture.get(3)), int(capture.get(4))))
 
+    # set up map display
     map_axes, frame_axes = gen_axes(mapping)
+    init_map(map_axes)
+    map_template = None
+    if map_file:
+        map_template = cv2.imread(map_file)
 
+    points_index = 0
     # frame_index is index + 1
     for frame_index in tqdm(range(1, num_frames + 1)):
         success, frame = capture.read()
@@ -205,33 +278,46 @@ def scan_video(video, opWrapper, mapping, sample_rate, video_output, output, dis
         keypoints = None
         if not fast:
             keypoints, _ = get_keypoints(opWrapper, frame)
-        # grab 1 frame a second
+        # grab point data
+        point_data = False
+        point_string = "Not pointing"
+        if points and points_index < len(points["frames"]) and points["frames"][points_index]["frame_number"] == frame_index:
+            point_data = True
+            point_x = points["frames"][points_index]["x"]
+            point_y = points["frames"][points_index]["y"]
+            points_index += 1
+        # grab frames at sample rate
         if frame_index % sample_rate == 0:
             # find image coords
-            user_x, user_y, user_frame_string, frame = find_user(opWrapper, frame, keypoints)
-            robot_x, robot_y, robot_frame_string, frame = find_robot(frame)
+            user_back_coords, user_front_coords, user_frame_string, frame = find_user(opWrapper, frame, keypoints)
+            robot_coords, robot_frame_string, frame = find_robot(frame)
             # find map coords
-            user_map_coords, user_map_string = calc_coords(mapping, user_x, user_y)
-            robot_map_coords, robot_map_string = calc_coords(mapping, robot_x, robot_y)
+            user_map_back_coords, user_map_string = calc_coords(mapping, user_back_coords)
+            user_map_front_coords, _ = calc_coords(mapping, user_front_coords)
+            robot_map_coords, robot_map_string = calc_coords(mapping, robot_coords)
+            # find point coords
+            point_coords = None
+            if point_data:
+                point_coords, point_string = calc_point_coords(user_map_back_coords, user_map_front_coords, points["grid_size"], point_x, point_y)
             # update frame
-            frame = write_coords(frame, user_frame_string, user_map_string, robot_frame_string, robot_map_string)
+            frame = write_coords(frame, user_frame_string, user_map_string, robot_frame_string, robot_map_string, point_string)
             frame = draw_axes(frame, frame_axes)
-            frame = draw_map(frame, map_axes, user_map_coords, robot_map_coords)
+            frame = draw_map(frame, map_template, user_map_back_coords, user_map_front_coords, robot_map_coords, point_coords)
+            # add to json
+            position = {}
+            position["frame_number"] = frame_index
+            position["user_coords"] = user_map_back_coords
+            position["robot_coords"] = robot_map_coords
+            if point_data:
+                position["point_coords"] = point_coords
+            positions.append(position)
             if video_output:
                 writer.write(frame)
-            if output:
-                outfile.write("Frame " + str(frame_index) + "\n")
-                outfile.write(user_frame_string + "\n")
-                outfile.write(user_map_string + "\n")
-                outfile.write(robot_frame_string + "\n")
-                outfile.write(robot_map_string + "\n")
-                outfile.write("\n")
             if display:
                 cv2.imshow("Frame", frame)
                 cv2.waitKey(1)
     print("")
-    if output:
-        outfile.close()
+    return positions
 
 
 # builds a mapping between camera plane and map plane
@@ -294,13 +380,15 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--video", help="video to run on")
     parser.add_argument("-c", "--coords", help="robot coordinates on map")
     parser.add_argument("-s", "--sample_rate", type=int, default=30, help="sample rate dictating frame skips")
-    parser.add_argument("-o", "--output", help="file to store results")
+    parser.add_argument("-o", "--output", help="file to store positions json")
     parser.add_argument("-vo", "--video_output", help="file to store modified video")
     parser.add_argument("-m", "--mapping", help="file to load pickled mapping from")
     parser.add_argument("-mo", "--mapping_output", help="file to store pickled mapping to")
     parser.add_argument("-mf", "--mapping_figure", help="file to store mapping estimate figure")
+    parser.add_argument("-mt", "--map_template", help="map template to draw on")
     parser.add_argument("-d", "--display", action="store_true", default=False, help="display frames as they are processed")
     parser.add_argument("-f", "--fast", action="store_true", default=False, help="speed up openpose at the cost of accuracy")
+    parser.add_argument("-p", "--points", help="file to load points json")
 
     args = parser.parse_args()
 
@@ -338,4 +426,9 @@ if __name__ == "__main__":
             pickle.dump(mapping, mapping_file)
 
     # scan video
-    scan_video(args.video, opWrapper, mapping, args.sample_rate, args.video_output, args.output, args.display, args.fast)
+    positions = scan_video(args.video, opWrapper, mapping, args.map_template, args.sample_rate, args.points, args.video_output, args.display, args.fast)
+
+    # write json file
+    if args.output:
+        with open(args.output, "w") as outfile:
+            json.dump(positions, outfile)
